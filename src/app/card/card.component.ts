706c8c5cad705207dd8cf3ae24c56867a8474fc9
kryban93 <kryban1993@gmail.com>
2022-06-08 19:59:08 +0200

    Updated html template

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  totalMonth: number = 42643892;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/data/expensesData.ts
@@ -0,0 +1,6 @@
+export const data = [
+  { date: new Date(), balance: 4242424 },
+  { date: new Date(), balance: 4243233 },
+  { date: new Date(), balance: 4241112 },
+  { date: new Date(), balance: 4555424 },
+];
